2cbb1a3390aba02c4972c498345a88d6ddeab729
FrezzyDevs <134243931+FrezzyDevs@users.noreply.github.com>
2023-07-16 07:38:32 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,16 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Total Roblox Drama | WeTheDevs", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
-
-local Tab = Window:MakeTab({
+OrionLib:MakeNotification({
+	Name = "UI",
+	Content = "The user interface (UI) has finished loading.",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+local Tab1 = Window:MakeTab({
 	Name = "🎮 | Player",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+local Section1 = Tab1:AddSection({
+	Name = "Players"
 })
