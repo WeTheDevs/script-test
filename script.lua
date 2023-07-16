0c82cd43755ab046ba643c80a1db03b7ebe320bd
FrezzyDevs <134243931+FrezzyDevs@users.noreply.github.com>
2023-07-16 07:11:47 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,2 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
