ef7ed18a3672fc3eb3b4daf731fd7fde9181f746
FrezzyDevs <134243931+FrezzyDevs@users.noreply.github.com>
2023-07-16 07:27:54 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,8 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Total Roblox Drama | WeTheDevs", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "🎮 | Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
