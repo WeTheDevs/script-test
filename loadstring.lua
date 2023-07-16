65ccce202e48c7629e2651c2eb8d3db60f88b42b
FrezzyDevs <134243931+FrezzyDevs@users.noreply.github.com>
2023-07-16 07:26:20 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WeTheDevs/script-test/main/script.lua?token=GHSAT0AAAAAACELJGXM4QOVGMEHQTXTITQKZFT2JKQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WeTheDevs/script-test/main/script.lua"))()
